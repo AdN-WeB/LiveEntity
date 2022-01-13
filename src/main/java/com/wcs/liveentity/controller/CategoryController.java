a2b417dc7c87ba318c92159fe6530398a4a71ee9
Pierre <53476143+RoYaLSleT@users.noreply.github.com>
2022-01-13 12:21:54 +0100

    Ajout get product from category

--- src/main/java/com/wcs/liveentity/controller/CategoryController.java
+++ src/main/java/com/wcs/liveentity/controller/CategoryController.java
@@ -1,15 +1,22 @@
 package com.wcs.liveentity.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.wcs.liveentity.dto.CategoryDto;
 import com.wcs.liveentity.model.Category;
+import com.wcs.liveentity.model.Product;
 import com.wcs.liveentity.repository.CategoryRepository;
 
 @RestController
@@ -19,11 +26,25 @@ public class CategoryController {
 	@Autowired
 	CategoryRepository categoryRepository;
 	
+	@GetMapping
+	public List<Category> getAll(){
+		return categoryRepository.findAll();
+	}
+	
 	@PostMapping
 	public Category create(@Valid @RequestBody CategoryDto categoryDto) {
 		Category category = new Category();
 		category.setName(categoryDto.getName());
 		category.setDisplayOrder(categoryDto.getDisplayOrder());
 		return categoryRepository.save(category);
+	}
+	
+	@GetMapping("/{id}/products")
+	public List<Product> getProductsFromCategory(@PathVariable(required = true) Long id){
+		// on va chercher la category, si elle n'existe pas, on lance une exception
+		Category category = categoryRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		// on retourne la liste de produits liée à la catégorie.
+		return category.getProducts();
 	}
 }
--- src/main/java/com/wcs/liveentity/model/Category.java
+++ src/main/java/com/wcs/liveentity/model/Category.java
@@ -13,6 +13,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Category {
 
@@ -29,6 +31,7 @@ public class Category {
 	@Column(name= "display_order")
 	private Integer displayOrder;
 	
+	@JsonIgnore
 	@ManyToMany(mappedBy = "categories")
 	private List<Product> products;
 
